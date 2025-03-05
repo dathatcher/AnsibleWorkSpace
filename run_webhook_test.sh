e7de98c6e8835f1ff69db3c3a36d356bd3976228
David A. Thatcher <david.austin.thatcher@gmail.com>
2025-03-05 13:21:45 -0500

    Update run_webhook_test.sh

--- run_webhook_test.sh
+++ run_webhook_test.sh
@@ -1,4 +1,8 @@
 #setting local environment variables that will simulate Ansible Secrets, where they will reside in iteration 2
+#This also assumes you have Ansible installed. I used GitHUb spaces, so I needed to load Ansible on my image
+# pip3 install --user ansible
+# pip3 install --user ansible
+# ansible --version 
 export JIRA_API_TOKEN=""
 export JIRA_USER_EMAIL=""
 export TO_EMAIL=""
