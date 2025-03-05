78bb50a3f68a4012a9556b7b8a456b909f52410d
David A. Thatcher <david.austin.thatcher@gmail.com>
2025-03-05 13:19:28 -0500

    Update set_webhook_payload.sh

--- set_webhook_payload.sh
+++ set_webhook_payload.sh
@@ -1,1 +1,2 @@
+#Simulates a simple payload that would be sent from GitHUb on the Pull Requests from the Jira named branch to QA
 echo '{ "pull_request": { "head": { "ref": "SCRUM-1" }, "base": { "ref": "QA" }, "merged": true }}' > /tmp/webhook_payload.json
